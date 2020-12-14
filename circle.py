f4bc88cdf87eeb5a7d9c07f5da6d7366d3544a75
Mynadevi <75661805+Mynadevi@users.noreply.github.com>
2020-12-14 13:23:00 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 # area-of-circle
 print("Input the radius of the circle");
-val=input();
-area=float(val)* float(val) * 3.141592658979;
+val=float(input());
+area=val* val * 3.141592658979;
 print("The area of the circle with radius",val,"is: ",area);
